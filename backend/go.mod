ce330c620f9d2f1233af44f7b243b9a82f748a21
DoHyeon Lee <cailhuiris@gmail.com>
2020-12-18 23:39:26 +0900

    add: frontend & backend skeleton

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module backend
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ backend/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Server is running at 8080 port")
+	})
+
+	r.Run()
+}
